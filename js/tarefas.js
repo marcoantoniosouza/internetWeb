71751d4b4564fddef6f7107c55c1babf1564a77a
Marco Antonio <marcoaps.19@gmail.com>
2016-04-28 10:34:33 -0300

    Menu dropdown e tarefas salvas na sessionStorage

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,10 +1,13 @@
-var listaTarefas = ["abc","123"];
-
 function show_alert(x){
 	alert(x);
 }
 
 function atualizar_lista(){
+	listaTarefas = JSON.parse(sessionStorage.getItem('tarefas'));
+	if (listaTarefas==null){
+		listaTarefas = ["Adicione novas tarefas"];
+		sessionStorage.setItem('tarefas', JSON.stringify(listaTarefas));
+	}
 	var out = "<tr><td>TAREFA</td></tr>"
 	for (var i = listaTarefas.length - 1; i >= 0; i--) {
 		out += "<tr><td>" + listaTarefas[i] + "</td></tr>"
@@ -13,6 +16,7 @@ function atualizar_lista(){
 }
 
 function adicionar_item(descricao){
+	listaTarefas = JSON.parse(sessionStorage.getItem('tarefas'));
 	if(descricao==""){
 		alert("Descricao Inválida");
 	}else{
@@ -23,6 +27,7 @@ function adicionar_item(descricao){
 			}
 		};
 		listaTarefas.push(descricao);
+		sessionStorage.setItem('tarefas', JSON.stringify(listaTarefas));
 		atualizar_lista();
 	}
 }
